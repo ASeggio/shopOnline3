7c841337a89721d5b5af0c6bc24ec320a87f4228
marinatarullo <tarullomaria@libero.it>
2018-05-04 15:44:36 +0200

    ra

--- shopOnline3/src/it/accenture/controller/ListaAcquisti.java
+++ shopOnline3/src/it/accenture/controller/ListaAcquisti.java
@@ -4,19 +4,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import it.accenture.dao.AcquistoDaoImpl;
+import it.accenture.dao.ProdottoDaoImpl;
 import it.accenture.model.Acquisto;
+import it.accenture.model.Prodotto;
 
 public class ListaAcquisti extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		List<Acquisto> listaAcquisti = new ArrayList<>();
-		 
+		AcquistoDaoImpl acquistoService = new AcquistoDaoImpl();
+		listaAcquisti = acquistoService.getAll();
+		for (Acquisto acquisto : listaAcquisti) {
+			System.out.println(acquisto); 
 	}
-
+	
+	req.setAttribute("listaAcquisti", listaAcquisti);
+	RequestDispatcher dispatcher = req.getRequestDispatcher("listaAcquisti.jsp");
+	dispatcher.forward(req, resp);
+}
 }
